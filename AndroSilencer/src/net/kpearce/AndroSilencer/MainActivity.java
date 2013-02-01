f503a8e31612843fc08541db991a8c3ec6bd2792
Kurtis Pearce <kurtis.pearce@gmail.com>
2013-01-31 21:15:21 -0600

    renamed some packages and deleted some oddities

---
+++ AndroSilencer/src/net/kpearce/AndroSilencer/MainActivity.java
@@ -0,0 +1,30 @@
+package net.kpearce.AndroSilencer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import com.example.AndroSilencer.R;
+
+public class MainActivity extends Activity {
+
+    private LinearLayout switchView;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        switchView = (LinearLayout) this.findViewById(R.id.switchLayout);
+    }
+
+    public void newLocationClick(View view){
+        Intent newLocationIntent = new Intent(view.getContext(),LocationTypeActivity.class);
+        startActivity(newLocationIntent);
+    }
+}
---
+++ AndroSilencer/src/net/kpearce/AndroSilencer/WifiScanResultsActivity.java
@@ -0,0 +1,58 @@
+package net.kpearce.AndroSilencer;
+
+import android.*;
+import android.R;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.text.AndroidCharacter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kurtis
+ * Date: 1/31/13
+ * Time: 12:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WifiScanResultsActivity extends ListActivity {
+
+//    private ListView scanResultsListView;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        setListAdapter(new ScanResultAdapter(this, R.layout.simple_list_item_1,wifiManager.getScanResults()));
+    }
+
+    class ScanResultAdapter extends ArrayAdapter<ScanResult>{
+
+        public ScanResultAdapter(Context context, int textViewResourceId, List<ScanResult> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ScanResult scanResult = getItem(position);
+            LayoutInflater inflater = (LayoutInflater) getContext()
+            			.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            TextView viewById = (TextView) inflater.inflate(R.layout.simple_list_item_1,parent,false);
+            viewById.setText(scanResult.SSID);
+            return viewById;
+        }
+    }
+}
