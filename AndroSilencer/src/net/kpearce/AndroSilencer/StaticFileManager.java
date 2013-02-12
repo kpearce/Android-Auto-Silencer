9504dfee46af89d45e8b5912c8809f437de8ec00
kpearce <kurtis.pearce@gmail.com>
2013-02-12 01:02:50 -0600

    added filemanager

---
+++ AndroSilencer/src/net/kpearce/AndroSilencer/StaticFileManager.java
@@ -0,0 +1,59 @@
+package net.kpearce.AndroSilencer;
+
+import android.content.Context;
+import com.example.AndroSilencer.R;
+
+import java.io.*;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kurtis
+ * Date: 2/12/13
+ * Time: 12:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StaticFileManager {
+
+    public static LinkedList<String> getSavedSSIDs(Context context) throws IOException {
+        File file = new File(context.getFilesDir(), context.getString(R.string.wifi_saves_file));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        LinkedList<String> ssids = new LinkedList<String>();
+        String ssid = bufferedReader.readLine();
+        while (ssid != null){
+            if(!ssids.contains(ssid)){
+                ssids.add(ssid);
+            }
+            ssid = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+        return ssids;
+    }
+
+    public static void saveSSID(Context context,String toSave) throws IOException {
+        if(getSavedSSIDs(context).contains(toSave)){
+            return;
+        }
+        FileOutputStream outputStream = context.openFileOutput(context.getString(R.string.wifi_saves_file),Context.MODE_APPEND);
+        PrintWriter writer = new PrintWriter(outputStream);
+        writer.println(toSave);
+        writer.flush();
+        writer.close();
+    }
+
+    public static void removeSSID(Context context, String toRemove) throws IOException {
+        LinkedList<String> currentSSIDs = getSavedSSIDs(context);
+        if(!currentSSIDs.contains(toRemove)){
+            return;
+        }
+
+        currentSSIDs.remove(toRemove);
+        FileWriter writer = new FileWriter(new File(context.getFilesDir(),context.getString(R.string.wifi_saves_file)),false);
+        PrintWriter printWriter = new PrintWriter(writer);
+        for (String currentSSID : currentSSIDs) {
+            printWriter.println(currentSSID);
+        }
+        printWriter.flush();
+        printWriter.close();
+    }
+}
